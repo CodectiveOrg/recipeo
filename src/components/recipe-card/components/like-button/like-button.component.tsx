6cab647f57f7eb33d7b7ab451acccd0f3b43c690
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-10 09:56:56 +0330

    [ADD] separate components for buttons

---
+++ src/components/recipe-card/components/like-button/like-button.component.tsx
@@ -0,0 +1,59 @@
+import type { MouseEvent, ReactNode } from "react";
+
+import { useMutation } from "@tanstack/react-query";
+
+import { toast } from "react-toastify";
+
+import clsx from "clsx";
+
+import { likeRecipeApi } from "@/api/recipe/like-recipe.api.ts";
+
+import IconButtonComponent from "@/components/icon-button/icon-button.component.tsx";
+import IconComponent from "@/components/icon/icon.component.tsx";
+
+import styles from "./like-button.module.css";
+
+type Props = {
+  className?: string;
+  recipeId: number;
+  liked?: boolean;
+};
+
+export default function LikeButtonComponent({
+  className,
+  recipeId,
+  liked,
+}: Props): ReactNode {
+  const { mutateAsync } = useMutation({
+    mutationKey: ["recipe-card", recipeId],
+    mutationFn: likeRecipeApi,
+    onError: (error): void => {
+      toast.error(error.message);
+    },
+    onSuccess: () => {},
+  });
+
+  const handleLikeButtonClick = async (
+    e: MouseEvent<HTMLButtonElement>,
+  ): Promise<void> => {
+    e.stopPropagation();
+
+    await mutateAsync({
+      id: recipeId,
+      action: liked ? "unlike" : "like",
+    });
+  };
+
+  return (
+    <IconButtonComponent
+      className={clsx(styles["like-button"], className)}
+      onClick={handleLikeButtonClick}
+    >
+      {liked ? (
+        <IconComponent className={styles.liked} name="heart-bold" />
+      ) : (
+        <IconComponent className={styles.unliked} name="heart-linear" />
+      )}
+    </IconButtonComponent>
+  );
+}
--- src/components/recipe-card/recipe-card.component.tsx
+++ src/components/recipe-card/recipe-card.component.tsx
@@ -1,15 +1,10 @@
-import type { MouseEvent, ReactNode } from "react";
+import type { ReactNode } from "react";
 
 import { Link } from "react-router";
 
-import { useMutation } from "@tanstack/react-query";
-
-import { toast } from "react-toastify";
-
-import { likeRecipeApi } from "@/api/recipe/like-recipe.api";
-
-import IconButtonComponent from "@/components/icon-button/icon-button.component";
 import IconComponent from "@/components/icon/icon.component";
+import ArrowButtonComponent from "@/components/recipe-card/components/arrow-button/arrow-button.component.tsx";
+import LikeButtonComponent from "@/components/recipe-card/components/like-button/like-button.component.tsx";
 import TypographyComponent from "@/components/typography/typography.component";
 
 import { Recipe } from "@/entities/recipe";
@@ -23,44 +18,14 @@ type Props = {
 };
 
 export default function RecipeCardComponent({ recipe }: Props): ReactNode {
-  const { mutateAsync } = useMutation({
-    mutationKey: ["recipe-card", recipe.id],
-    mutationFn: likeRecipeApi,
-    onError: (error): void => {
-      toast.error(error.message);
-    },
-    onSuccess: () => {},
-  });
-
-  const handleLikeButtonClick = async (
-    e: MouseEvent<HTMLButtonElement>,
-  ): Promise<void> => {
-    e.stopPropagation();
-
-    await mutateAsync({
-      id: recipe.id,
-      action: recipe.isLikedByCurrentUser ? "unlike" : "like",
-    });
-  };
-
   return (
     <Link className={styles["recipe-card"]} to={`/recipe/${recipe.id}`}>
       <div className={styles.content}>
         <img
           className={styles["recipe-picture"]}
-          src={recipe.picture || "/placeholders/featured.webp"}
+          src={recipe.picture || "/placeholders/recipe.webp"}
           alt=""
         />
-        <IconButtonComponent
-          className={styles.like}
-          onClick={handleLikeButtonClick}
-        >
-          {recipe.isLikedByCurrentUser ? (
-            <IconComponent className={styles.liked} name="heart-bold" />
-          ) : (
-            <IconComponent className={styles.unliked} name="heart-linear" />
-          )}
-        </IconButtonComponent>
         <TypographyComponent
           as="p"
           className={styles.title}
@@ -75,24 +40,22 @@ export default function RecipeCardComponent({ recipe }: Props): ReactNode {
             src={recipe.user.picture || "/placeholders/user.svg"}
             alt="User's Profile Picture"
           />
-          <TypographyComponent
-            as="span"
-            ellipsis
-            className={styles.username}
-            variant="s"
-          >
+          <TypographyComponent as="span" ellipsis variant="s">
             {recipe.user.username}
           </TypographyComponent>
         </span>
-        <IconButtonComponent className={styles.arrow}>
-          <IconComponent name="map-arrow-right-bold" />
-        </IconButtonComponent>
         <span className={styles.duration}>
           <IconComponent name="clock-circle-outline" />
           <TypographyComponent as="span" variant="s">
             {formatDuration(recipe.duration)}
           </TypographyComponent>
         </span>
+        <LikeButtonComponent
+          className={styles["like-button"]}
+          recipeId={recipe.id}
+          liked={recipe.isLikedByCurrentUser}
+        />
+        <ArrowButtonComponent className={styles["arrow-button"]} />
       </div>
     </Link>
   );
