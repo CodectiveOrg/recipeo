da9fd66b67e9c6f242fe02c4f3e9f6769fe74a04
ZahraZare <zahrazare3099@gmail.com>
2025-09-13 12:55:41 +0330

    refactor dto

--- src/dto/response/recentRecipes.response.dto.ts
+++ src/dto/response/recentRecipes.response.dto.ts
@@ -1,6 +1,5 @@
 import type { Recipe } from "@/entities/recipe";
 
 import type { PaginatedResponseDto } from "./paginated.response.dto";
-import type { ResponseDto } from "./response.dto";
 
 export type RecentRecipesResponseDto = PaginatedResponseDto<Recipe>;
