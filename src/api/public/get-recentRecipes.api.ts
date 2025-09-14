98c5630703f6658b6a8a19ba408c30ea2ebfab37
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-14 11:31:22 +0330

    [ADD] recent recipes to home page

--- src/api/public/get-recentRecipes.api.ts
+++ src/api/public/get-recentRecipes.api.ts
@@ -3,7 +3,7 @@ import type { RecentRecipesResponseDto } from "@/dto/response/recentRecipes.resp
 import { richFetch } from "@/utils/fetch.utils.ts";
 
 export async function getRecentRecipesApi(): Promise<RecentRecipesResponseDto> {
-  const data = await richFetch<RecentRecipesResponseDto>("/recipe/popular");
+  const data = await richFetch<RecentRecipesResponseDto>("/recipe/recent");
 
   if ("error" in data) {
     throw new Error(data.error);
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,20 +1,61 @@
 import { type ReactNode } from "react";
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 import HandfulSection from "@/sections/handful/handful.section.tsx";
+import RecentRecipesSection from "@/sections/recent-recipes/recent-recipes.section.tsx";
 
 import { getPopularRecipesApi } from "@/api/public/get-popular-recipes.api.ts";
+import { getRecentRecipesApi } from "@/api/public/get-recentRecipes.api.ts";
 
 import RecipesCarouselComponent from "@/components/recipes-carousel/recipes-carousel.component.tsx";
 import TagsCarouselComponent from "@/components/tags-carousel/tags-carousel.component.tsx";
 
+import type { RecentRecipesResponseDto } from "@/dto/response/recentRecipes.response.dto.ts";
+
 import styles from "./home.module.css";
+
+const PAGE_SIZE = 5;
+
+type FetchPageType = {
+  pageParam: number;
+};
 
 export default function HomePage(): ReactNode {
   const popularRecipesQueryResult = useQuery({
     queryKey: ["recipes", "popular"],
     queryFn: getPopularRecipesApi,
+  });
+
+  const fetchPage = async ({
+    pageParam = 0,
+  }: FetchPageType): Promise<RecentRecipesResponseDto> => {
+    const all = await getRecentRecipesApi();
+    const start = pageParam;
+    const end = start + PAGE_SIZE;
+    const pageItems = all.items.slice(start, end);
+    const currentPage = Math.floor(start / PAGE_SIZE) + 1;
+    const lastPage = Math.ceil(all.items.length / PAGE_SIZE);
+    return { items: pageItems, lastPage, currentPage };
+  };
+
+  const {
+    data: recentRecipes,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["recent-recipes"],
+    queryFn: ({ pageParam = 0 }) => fetchPage({ pageParam }),
+    getNextPageParam: (last) => {
+      if (last.currentPage < last.lastPage) {
+        return last.currentPage * PAGE_SIZE;
+      }
+      return undefined;
+    },
+
+    initialPageParam: 0,
   });
 
   return (
@@ -35,6 +76,14 @@ export default function HomePage(): ReactNode {
             size="small"
           />
         </HandfulSection>
+        <br />
+        <RecentRecipesSection
+          recentRecipes={recentRecipes}
+          fetchNextPage={fetchNextPage}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+          status={status}
+        />
       </main>
     </div>
   );
