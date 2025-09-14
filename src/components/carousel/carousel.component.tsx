3ee57cd93cb3c537a08b77a942178fb396619227
Reza Mazaheri <163832802+mazaherireza@users.noreply.github.com>
2025-09-14 11:22:39 +0330

    54 Tags Section (#28)
    * [ADD] tag
    
    * [ADD] categories
    
    * [DELETE] unused component
    
    * [ADD] api
    
    * [ADD] section file
    
    * [ADD] state
    
    * [UPDATE]
    
    * [UPDATE]
    
    * [UPDATE]
    
    * [REFACTOR] add handful section
    
    * [REFACTOR] tags carousel
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

--- src/components/carousel/carousel.component.tsx
+++ src/components/carousel/carousel.component.tsx
@@ -1,20 +1,28 @@
 import type { ReactNode } from "react";
 
 import { FreeMode } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, type SwiperProps, SwiperSlide } from "swiper/react";
 
 import "@/styles/swiper.css";
 
 import styles from "./carousel.module.css";
 
 type Props = {
+  spaceBetween?: SwiperProps["spaceBetween"];
   children: ReactNode[];
 };
 
-export default function CarouselComponent({ children }: Props): ReactNode {
+export default function CarouselComponent({
+  spaceBetween,
+  children,
+}: Props): ReactNode {
   return (
     <div className={styles.carousel}>
-      <Swiper slidesPerView="auto" modules={[FreeMode]}>
+      <Swiper
+        slidesPerView="auto"
+        modules={[FreeMode]}
+        spaceBetween={spaceBetween}
+      >
         {children.map((child, index) => (
           <SwiperSlide key={index} className={styles.slide}>
             {child}
