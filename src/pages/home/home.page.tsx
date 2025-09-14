162075eae033daef06e28f721ac8fc04d468bd2c
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-14 10:37:53 +0330

    [REFACTOR] to recipes carousel

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,17 +1,35 @@
 import { type ReactNode } from "react";
 
-import PopularRecipesSection from "@/sections/popular-recipes/popular-recipes.section";
+import { useQuery } from "@tanstack/react-query";
+
+import RecipesCarouselSection from "@/sections/recipes-carousel/recipes-carousel.section.tsx";
+
+import { getPopularRecipesApi } from "@/api/public/get-popularRecipes.api.ts";
 
 import styles from "./home.module.css";
 
 export default function HomePage(): ReactNode {
+  const getPopularRecipesQueryResult = useQuery({
+    queryKey: ["popular-recipes"],
+    queryFn: getPopularRecipesApi,
+  });
+
   return (
     <div className={styles.home}>
       <header>Header</header>
       <main>
-        <PopularRecipesSection />
+        <RecipesCarouselSection
+          title="Popular Recipes"
+          viewAllHref="/popular"
+          queryResult={getPopularRecipesQueryResult}
+        />
         <br />
-        <PopularRecipesSection size="small" />
+        <RecipesCarouselSection
+          title="Popular Recipes"
+          viewAllHref="/popular"
+          queryResult={getPopularRecipesQueryResult}
+          size="small"
+        />
       </main>
     </div>
   );
--- src/sections/recipes-carousel/recipes-carousel.section.tsx
+++ src/sections/recipes-carousel/recipes-carousel.section.tsx
@@ -2,29 +2,34 @@ import type { ReactNode } from "react";
 
 import { Link } from "react-router";
 
-import { useQuery } from "@tanstack/react-query";
+import { type UseQueryResult } from "@tanstack/react-query";
 
 import clsx from "clsx";
-
-import { getPopularRecipesApi } from "@/api/public/get-popularRecipes.api";
 
 import CarouselComponent from "@/components/carousel/carousel.component";
 import RecipeCardComponent from "@/components/recipe-card/recipe-card.component";
 import TypographyComponent from "@/components/typography/typography.component";
 
-import styles from "./popular-recipes.module.css";
+import type { PaginatedResponseDto } from "@/dto/response/pagination.response.dto.ts";
+
+import type { Recipe } from "@/entities/recipe.ts";
+
+import styles from "./recipes-carousel.module.css";
 
 type Props = {
+  title: string;
+  viewAllHref?: string;
+  queryResult: UseQueryResult<PaginatedResponseDto<Recipe>>;
   size?: "medium" | "small";
 };
 
-export default function PopularRecipesSection({
+export default function RecipesCarouselSection({
+  title,
+  viewAllHref,
+  queryResult,
   size = "medium",
 }: Props): ReactNode {
-  const { data, isPending, isError } = useQuery({
-    queryKey: ["popular-recipes"],
-    queryFn: getPopularRecipesApi,
-  });
+  const { data, isPending, isError } = queryResult;
 
   if (isPending) {
     return <>Loading...</>;
@@ -35,12 +40,12 @@ export default function PopularRecipesSection({
   }
 
   return (
-    <div className={styles["popular-recipes"]}>
+    <div className={styles["recipes-carousel"]}>
       <div className={styles["header"]}>
         <TypographyComponent as="h1" variant="h1">
-          Popular Recipes
+          {title}
         </TypographyComponent>
-        <Link to="#">View All</Link>
+        {viewAllHref && <Link to={viewAllHref}>View All</Link>}
       </div>
       <CarouselComponent>
         {data.items.map((recipe) => (
