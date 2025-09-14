dec5a46310c148fd338421d3016c6a1a15369cde
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-14 10:44:55 +0330

    [RENAME] api

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import RecipesCarouselSection from "@/sections/recipes-carousel/recipes-carousel.section.tsx";
 
-import { getPopularRecipesApi } from "@/api/public/get-popularRecipes.api.ts";
+import { getPopularRecipesApi } from "@/api/public/get-popular-recipes.api.ts";
 
 import styles from "./home.module.css";
 
