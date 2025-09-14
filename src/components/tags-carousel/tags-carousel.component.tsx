55cbd9f3f3c93067696b4504a8d4267e31da077d
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-14 11:21:01 +0330

    [REFACTOR] tags carousel

--- src/components/tags-carousel/tags-carousel.component.tsx
+++ src/components/tags-carousel/tags-carousel.component.tsx
@@ -1,4 +1,6 @@
-import { type ReactNode, useState } from "react";
+import { type ReactNode } from "react";
+
+import { Link } from "react-router";
 
 import { useQuery } from "@tanstack/react-query";
 
@@ -6,53 +8,39 @@ import { getAllTagsApi } from "@/api/tag/get-all-tags.api.ts";
 
 import ButtonComponent from "@/components/button/button.component.tsx";
 import CarouselComponent from "@/components/carousel/carousel.component.tsx";
+import LoadingComponent from "@/components/loading/loading.component.tsx";
+
+import type { Tag } from "@/entities/tag.ts";
 
 export default function TagsCarouselComponent(): ReactNode {
-  const [selectedTags, setSelectedTags] = useState<number[]>([]);
-
-  const { data } = useQuery({
-    queryKey: ["tags-section"],
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["tags"],
     queryFn: getAllTagsApi,
   });
 
-  const isSelected = (tagId: number): boolean => {
-    const index = selectedTags.findIndex((id) => id === tagId);
-    return index >= 0 ? true : false;
-  };
+  if (isPending) {
+    return <LoadingComponent />;
+  }
 
-  const handleButtonToggleSelection = (tagId: number): void => {
-    const index = selectedTags.findIndex((id) => id === tagId);
+  if (isError) {
+    return <>Error...</>;
+  }
 
-    if (index >= 0) {
-      setSelectedTags((old) => old.filter((id) => id !== tagId));
-    } else {
-      setSelectedTags((old) => [...old, tagId]);
-    }
-  };
+  const tags: Tag[] = [{ id: -1, title: "All" }, ...data.slice(0, 5)];
 
-  const content = data
-    ? data.map((tag) => (
+  return (
+    <CarouselComponent spaceBetween={16}>
+      {tags.map((tag) => (
         <ButtonComponent
           key={tag.id}
+          as={Link}
+          to={`/tag/${tag.title}`}
           size="small"
-          variant={isSelected(tag.id) ? "solid" : "outlined"}
-          onClick={() => handleButtonToggleSelection(tag.id)}
+          variant={tag.title === "All" ? "solid" : "outlined"}
         >
           {tag.title}
         </ButtonComponent>
-      ))
-    : [];
-
-  return (
-    <CarouselComponent>
-      <ButtonComponent
-        size="small"
-        variant={isSelected(-1) ? "solid" : "outlined"}
-        onClick={() => handleButtonToggleSelection(-1)}
-      >
-        All
-      </ButtonComponent>
-      {content}
+      ))}
     </CarouselComponent>
   );
 }
