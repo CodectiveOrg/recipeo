79121584ac32bb04641f65ba292a89e29ea4389d
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-14 17:32:07 +0330

    [ADD] store

--- src/stores/search-history.store.ts
+++ src/stores/search-history.store.ts
@@ -1,28 +1,33 @@
 import { create } from "zustand/react";
 
-import type { SearchHistoryType } from "@/types/search-history.type";
+import type { SearchHistoryType } from "@/types/search-history.type.ts";
 
 type SearchHistoryStore = {
   list: SearchHistoryType[];
-  addToList: (item: SearchHistoryType) => void;
+  add: (item: SearchHistoryType) => void;
   remove: (id: number) => void;
+  reset: () => void;
 };
 
-const save = (lists: SearchHistoryType[]):void => {
-   localStorage.setItem("search-history", JSON.stringify(lists))
-}
+const save = (lists: SearchHistoryType[]): void => {
+  localStorage.setItem("search-history", JSON.stringify(lists));
+};
 
 const load = (): SearchHistoryType[] => {
   const item = localStorage.getItem("search-history");
-  if(! item) {
-    return []
+  if (!item) {
+    return [];
   }
 
   return JSON.parse(item);
-}
+};
 
 export const useSearchHistory = create<SearchHistoryStore>((set) => ({
-  list: load(),
-  addToList: (item) =>set(state => ({ state.list: [...state.list, item]})),
-  remove: (id) => set(state => ({})),
+  list: [],
+  add: (item) => {
+    set((state) => ({ list: [...state.list, item] }));
+  },
+  remove: (id) =>
+    set((state) => ({ list: state.list.filter((item) => item.id !== id) })),
+  reset: () => set({ list: [] }),
 }));
