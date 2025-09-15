95df16b7fed72008233376c4a77d509409cb959e
ZahraZare <zahrazare3099@gmail.com>
2025-09-15 11:04:15 +0330

    create dnd provider

---
+++ src/providers/dndProvider/dnd.provider.tsx
@@ -0,0 +1,14 @@
+import { type PropsWithChildren, type ReactNode } from "react";
+
+import {
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+
+type Props = PropsWithChildren;
+export default function DndProvider({ children }: Props): ReactNode {
+  const sensors = useSensors(useSensor(PointerSensor));
+  return <DndContext sensors={sensors}>{children}</DndContext>;
+}
