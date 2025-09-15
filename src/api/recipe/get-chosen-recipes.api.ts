8444b94d372afd9a09d0696fba2f48a72bf4ec18
Reza Mazaheri <163832802+mazaherireza@users.noreply.github.com>
2025-09-15 15:12:10 +0330

    49  Editor's Choice Section (#32)
    * [ADD] editor's choice section
    
    * [UPDATE]
    
    * [REFACTOR] chosen recipes
    
    * [REMOVE] extra gap
    
    * [REMOVE] redundant code
    
    * [FIX] wrong href
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

---
+++ src/api/recipe/get-chosen-recipes.api.ts
@@ -0,0 +1,13 @@
+import type { PaginatedRecipesResponseDto } from "@/dto/response/paginated-recipes.response.dto.ts";
+
+import { richFetch } from "@/utils/fetch.utils";
+
+export async function getChosenRecipesApi(): Promise<PaginatedRecipesResponseDto> {
+  const data = await richFetch<PaginatedRecipesResponseDto>("/recipe/chosen");
+
+  if ("error" in data) {
+    throw new Error(data.error);
+  }
+
+  return data.result;
+}
