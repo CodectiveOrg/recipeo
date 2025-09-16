6f3681c7525a36616e12add37ba9a828baccae0f
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-09-16 15:16:03 +0330

    [PRETTIER]

--- src/pages/tags/tags.page.tsx
+++ src/pages/tags/tags.page.tsx
@@ -7,10 +7,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getAllTagsApi } from "@/api/tag/get-all-tags.api";
 
 import ButtonComponent from "@/components/button/button.component";
+import IconButtonComponent from "@/components/icon-button/icon-button.component.tsx";
 import IconComponent from "@/components/icon/icon.component";
 import LoadingComponent from "@/components/loading/loading.component";
 import TypographyComponent from "@/components/typography/typography.component";
-import IconButtonComponent from "@/components/icon-button/icon-button.component.tsx";
 
 import styles from "./tags.module.css";
 
@@ -35,10 +35,15 @@ export default function TagsPage(): ReactNode {
   return (
     <div className={styles["tags"]}>
       <header>
-        <IconButtonComponent className={styles['back-button']} onClick={() => navigate(-1)}>
-          <IconComponent name="alt-arrow-left-linear"/>
+        <IconButtonComponent
+          className={styles["back-button"]}
+          onClick={() => navigate(-1)}
+        >
+          <IconComponent name="alt-arrow-left-linear" />
         </IconButtonComponent>
-        <TypographyComponent variant="h2" className={styles.title}>Tags</TypographyComponent>
+        <TypographyComponent variant="h2" className={styles.title}>
+          Tags
+        </TypographyComponent>
       </header>
       <main>
         <ul>
