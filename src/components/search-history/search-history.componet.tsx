0ac43bee6155de889b22ac5f2fc6fffb767f44e9
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-17 14:49:11 +0330

    [ADD] clear button

--- src/components/search-history/search-history.componet.tsx
+++ src/components/search-history/search-history.componet.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router";
 
 import { useSearchHistoryStore } from "@/stores/search-history.store";
 
+import ButtonComponent from "@/components/button/button.component.tsx";
 import IconButtonComponent from "@/components/icon-button/icon-button.component.tsx";
 import IconComponent from "@/components/icon/icon.component.tsx";
 import TypographyComponent from "@/components/typography/typography.component.tsx";
@@ -15,32 +16,48 @@ import styles from "./search-history.module.css";
 export default function SearchHistoryComponent(): ReactNode {
   const items = useSearchHistoryStore((state) => state.items);
   const remove = useSearchHistoryStore((state) => state.remove);
+  const clear = useSearchHistoryStore((state) => state.clear);
 
   return (
-    <ul className={styles["search-history"]}>
-      {items.map((item, index) => (
-        <li key={index}>
-          <Link to={generateSearchUrl(item)}>
-            <IconComponent name="clock-circle-linear" color="text-secondary" />
-            <TypographyComponent
-              as="span"
-              ellipsis
-              className={styles.title}
-              variant="p1"
-            >
-              {[item.query, item.tag, item.maxDuration]
-                .filter(Boolean)
-                .join(" - ")}
-            </TypographyComponent>
-          </Link>
-          <IconButtonComponent onClick={() => remove(index)}>
-            <IconComponent
-              name="trash-bin-trash-linear"
-              color="text-secondary"
-            />
-          </IconButtonComponent>
-        </li>
-      ))}
-    </ul>
+    <div className={styles["search-history"]}>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            <Link to={generateSearchUrl(item)}>
+              <IconComponent
+                name="clock-circle-linear"
+                color="text-secondary"
+              />
+              <TypographyComponent
+                as="span"
+                ellipsis
+                className={styles.title}
+                variant="p1"
+              >
+                {[item.query, item.tag, item.maxDuration]
+                  .filter(Boolean)
+                  .join(" - ")}
+              </TypographyComponent>
+            </Link>
+            <IconButtonComponent onClick={() => remove(index)}>
+              <IconComponent
+                name="trash-bin-trash-linear"
+                color="text-secondary"
+              />
+            </IconButtonComponent>
+          </li>
+        ))}
+      </ul>
+      {items.length > 0 && (
+        <ButtonComponent
+          variant="text"
+          color="secondary"
+          size="small"
+          onClick={clear}
+        >
+          Clear Search History
+        </ButtonComponent>
+      )}
+    </div>
   );
 }
