5d6a514f7ca7301535877104658810f473cd51aa
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-18 00:58:12 +0330

    [ADD] api

---
+++ src/api/recipe/search-recipes.api.ts
@@ -0,0 +1,21 @@
+import type { SearchRequestDto } from "@/dto/request/search.request.dto";
+import type { SearchRecipesResponseDto } from "@/dto/response/search-recipes.response.dto";
+
+import { richFetch } from "@/utils/fetch.utils";
+import { generateSearchUrl } from "@/utils/url.utils";
+
+export async function searchRecipesApi(
+  params: SearchRequestDto,
+): Promise<SearchRecipesResponseDto> {
+  const searchUrl = generateSearchUrl(params);
+
+  const data = await richFetch<SearchRecipesResponseDto>(
+    `/recipe${searchUrl}`,
+  );
+
+  if ("error" in data) {
+    throw new Error(data.error);
+  }
+
+  return data.result;
+}
