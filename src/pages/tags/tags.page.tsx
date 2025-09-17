a7e679a54d2415cca37546d5020b6b505b61a6cd
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-17 13:53:17 +0330

    [REFACTOR] tags page

--- src/pages/tags/tags.page.tsx
+++ src/pages/tags/tags.page.tsx
@@ -1,14 +1,13 @@
 import type { ReactNode } from "react";
 
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 
 import { useQuery } from "@tanstack/react-query";
 
 import { getAllTagsApi } from "@/api/tag/get-all-tags.api";
 
+import BackButtonComponent from "@/components/back-button/back-button.component.tsx";
 import ButtonComponent from "@/components/button/button.component";
-import IconButtonComponent from "@/components/icon-button/icon-button.component.tsx";
-import IconComponent from "@/components/icon/icon.component";
 import LoadingComponent from "@/components/loading/loading.component";
 import TypographyComponent from "@/components/typography/typography.component";
 
@@ -20,8 +19,6 @@ export default function TagsPage(): ReactNode {
     queryFn: getAllTagsApi,
   });
 
-  const navigate = useNavigate();
-
   if (isPending) {
     return <LoadingComponent />;
   }
@@ -30,30 +27,24 @@ export default function TagsPage(): ReactNode {
     return <>Error...</>;
   }
 
-  const clone = [{ id: -1, title: "All" }, ...data];
-
   return (
     <div className={styles["tags"]}>
       <header>
-        <IconButtonComponent
-          className={styles["back-button"]}
-          onClick={() => navigate(-1)}
-        >
-          <IconComponent name="alt-arrow-left-linear" />
-        </IconButtonComponent>
+        <BackButtonComponent className={styles["back-button"]} />
         <TypographyComponent variant="h2" className={styles.title}>
           Tags
         </TypographyComponent>
       </header>
       <main>
         <ul>
-          {clone.map((tag) => (
+          {data.map((tag) => (
             <li key={tag.id}>
               <ButtonComponent
                 as={Link}
-                to={`/tag/${tag.title}`}
+                to={`/search?tag=${tag.title}`}
+                variant="solid"
+                color="secondary"
                 size="small"
-                variant={tag.title === "All" ? "solid" : "outlined"}
               >
                 {tag.title}
               </ButtonComponent>
