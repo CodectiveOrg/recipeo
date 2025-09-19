6043d8d961f9db22a621b7716e8413059334cf7a
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-20 00:53:24 +0330

    [ADD] picture picker component

---
+++ src/components/picture-picker/picture-picker.component.tsx
@@ -0,0 +1,70 @@
+import { type ChangeEvent, type ReactNode, useRef } from "react";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { toast } from "react-toastify";
+
+import clsx from "clsx";
+
+import { updateUserApi } from "@/api/user/update-user.api";
+
+import styles from "./picture-picker.module.css";
+
+type Props = {
+  picture: string | null;
+  className?: string;
+};
+
+export default function PicturePickerComponent({
+  picture,
+  className,
+}: Props): ReactNode {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationKey: ["edit-picture"],
+    mutationFn: updateUserApi,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: async (result) => {
+      await Promise.allSettled([
+        queryClient.invalidateQueries({ queryKey: ["verify"] }),
+        queryClient.invalidateQueries({ queryKey: ["user"] }),
+      ]);
+
+      toast.success(result.message);
+    },
+  });
+
+  const inputChangeHandler = async (
+    e: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    if (!formRef.current || !e.currentTarget.files) {
+      return;
+    }
+
+    const formData = new FormData(formRef.current);
+    await mutation.mutateAsync({ file: formData });
+  };
+
+  return (
+    <form ref={formRef} className={clsx(styles["picture-picker"], className)}>
+      <img
+        src={picture ?? "/placeholders/user.svg"}
+        alt="User's Profile Picture"
+      />
+      <label>
+        <span className={styles.icon}>+</span>
+        <input
+          type="file"
+          name="picture"
+          accept="image/png, image/jpeg"
+          onChange={inputChangeHandler}
+        />
+      </label>
+    </form>
+  );
+}
