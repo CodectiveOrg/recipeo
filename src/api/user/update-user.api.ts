6db9b207fa5563ff44eb8113a49df702ca08830b
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-20 00:55:20 +0330

    [ADD] api

--- src/api/user/update-user.api.ts
+++ src/api/user/update-user.api.ts
@@ -1,13 +1,19 @@
-import type { UpdateUserRequestDto } from "@/dto/request/update-user.response.dto.ts";
+import type { UpdateUserRequestDto } from "@/dto/request/update-user.request.dto";
 import type { ResponseDto } from "@/dto/response/response.dto.ts";
 
 import { richFetch } from "@/utils/fetch.utils.ts";
 
-export async function updateUserApi(
-  dto: UpdateUserRequestDto,
-): Promise<ResponseDto> {
+type Params = {
+  user?: UpdateUserRequestDto;
+  file?: FormData;
+};
+
+export async function updateUserApi({
+  user,
+  file,
+}: Params): Promise<ResponseDto> {
   return await richFetch("/user/", {
     method: "PATCH",
-    body: JSON.stringify(dto),
+    body: file ? file : JSON.stringify(user),
   });
 }
