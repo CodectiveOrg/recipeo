21c5996a3578be2c798c30ab50d6e71d0ef3cf52
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-20 12:04:32 +0330

    [ADD] empty state

--- src/pages/search/components/search-results/search-results.component.tsx
+++ src/pages/search/components/search-results/search-results.component.tsx
@@ -6,10 +6,13 @@ import { searchRecipesApi } from "@/api/recipe/search-recipes.api.ts";
 
 import LoadingComponent from "@/components/loading/loading.component.tsx";
 import RecipeCardComponent from "@/components/recipe-card/recipe-card.component.tsx";
+import TypographyComponent from "@/components/typography/typography.component.tsx";
 
 import CurrentFiltersComponent from "@/pages/search/components/current-filters/current-filters.component.tsx";
 
 import HandfulSection from "@/sections/handful/handful.section.tsx";
+
+import styles from "./search-results.module.css";
 
 type Props = {
   queryString: string;
@@ -40,6 +43,13 @@ export default function SearchResultsComponent({
             <RecipeCardComponent key={recipe.id} recipe={recipe} />
           ))}
         </ul>
+        {data.length === 0 && (
+          <div className={styles.empty}>
+            <TypographyComponent as="p" variant="s" color="text-secondary">
+              No recipes found!
+            </TypographyComponent>
+          </div>
+        )}
       </HandfulSection>
     </div>
   );
