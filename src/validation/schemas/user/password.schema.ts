97737b7c0e6d0fbd02fad7f632ef034be6463701
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-21 17:00:48 +0330

    [UPDATE] password schema

--- src/validation/schemas/user/password.schema.ts
+++ src/validation/schemas/user/password.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const PasswordSchema = z.preprocess(
   (val) => (val === "" ? undefined : val),
   z
-    .string()
+    .string("Password must be a string.")
     .min(4, "Password must be more than 4 characters long.")
     .max(32, "Password must be less than 32 characters long.")
     .optional(),
